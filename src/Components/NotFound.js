55030856243540867b4577b98cf5aa2d7dcf327c
Julian Muratori <julianmuratori@gmail.com>
2017-05-13 17:11:34 -0400

    inventory for vendor

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Router, Route, browserHistory, Link, Miss } from 'react-router';
 
-class Welcome extends React.Component {
+class NotFound extends React.Component {
   render() {
     return (
       <div className="mainSplash">
         <div className="splashOptions">
-          <h1>Welcome to YourOrder!</h1>
-          <h3>Choose an option to get started</h3>
+          <h1>Ooooooops this link is broken!</h1>
 
           <div className="splashButton">
-            <a href="<Supplier /">Supplier</a>
-            <a href="">Client</a>
+            <Link to="/Supplier">Supplier</Link>
+            <Link to="/Supplier">Client</Link>
           </div>
         </div>
       </div>
@@ -19,4 +18,4 @@ class Welcome extends React.Component {
   }
 }
 
-export default Welcome;
+export default NotFound;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Link } from 'react-router';
 import Welcome from './Components/Welcome';
 import Supplier from './Components/Supplier';
+import Inventory from './Components/Inventory';
+import NotFound from './Components/NotFound';
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.addItem = this.addItem.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+		this.removeOne = this.removeOne.bind(this);
+		this.addOne = this.addOne.bind(this);
+
+		// get initial state
+		this.state = {
+			inventoryItems: {
+				item1: {
+					name: "apple",
+					price: 1,
+					priceStyle: "pc",
+					quantity: 4,
+					notes: "Tastes great"
+				}
+			},
+			order: {}
+		};
+	}
+
+
+	// updates state
+	  addItem(item) {
+	    // makes a copy of state
+	   const newItems = this.state.inventoryItems;
+	    // add new item
+	    const timestamp = Date.now();
+	    newItems[`item-${timestamp}`] = item;
+	    // // set state
+	    this.setState({
+				inventoryItems: newItems
+			})
+	  }
+
+	// removes item from an order
+		removeItem(item) {
+			const newItems = this.state.inventoryItems;
+			delete newItems[item];
+			this.setState({
+				inventoryItems: newItems
+			})
+		}
+
+		// Removes a single item from a count
+		removeOne(item) {
+			const newItems = this.state.inventoryItems;
+			// let price = newItems[item].price;
+			if (newItems[item].quantity === 0) {
+				console.log("can't go negative")
+			} else {
+				newItems[item].quantity = newItems[item].quantity - 1;
+			}
+			this.setState({
+				inventoryItems: newItems
+			})
+		}
+
+		addOne(item) {
+			const newItems = this.state.inventoryItems;
+			newItems[item].quantity = (newItems[item].quantity + 1);
+			this.setState({
+				inventoryItems: newItems
+			})
+		}
+
+
 	render() {
 		return (
-			<div>
-				<Welcome />
-			</div>
+		<Supplier storeName="eat shit"
+			addItem={this.addItem}
+			removeItem={this.removeItem}
+			inventoryItems={this.state.inventoryItems}
+			addOne = {this.addOne}
+			removeOne = {this.removeOne}
+		/>
 
 		)
 	}
 }
 
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+<Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/Supplier" component={Supplier}/>
+    <Route path="/Restaurant" component={Supplier} />
+		{/* <Miss component={NotFound}/> */}
+</Router>, document.getElementById('app'));
